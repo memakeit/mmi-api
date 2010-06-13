9e91505deebc7a5ef0588405841b3b8bccf25986
Shawn Sprehe <memakeit@gmail.com>
2010-06-12 18:35:21 -0600

    added basic authentication api

---
+++ classes/kohana/mmi/api/basic.php
@@ -0,0 +1,30 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Make API calls using basic authentication.
+ *
+ * @package     MMI API
+ * @author      Me Make It
+ * @copyright   (c) 2010 Me Make It
+ * @license     http://www.memakeit.com/license
+ */
+abstract class Kohana_MMI_API_Basic extends MMI_API
+{
+    /**
+     * Get the string to be sent in the authorization header.
+     *
+     * @return  string
+     */
+    protected function _get_auth_string()
+    {
+        $auth_config = $this->_auth_config;
+        $username = Arr::get($auth_config, 'username');
+        $password = Arr::get($auth_config, 'password');
+
+        $auth_header = '';
+        if ( ! empty($username) AND ! empty($password))
+        {
+            $auth_header = 'Basic '.base64_encode($username.':'.$password);
+        }
+        return $auth_header;
+    }
+} // End Kohana_MMI_API_Basic
