86c2fae85603197302e0e1d1fde26e62a37b96df
Shawn Sprehe <memakeit@gmail.com>
2010-06-12 18:42:35 -0600

    moved user-agent to the config file

--- classes/mmi/api.php
+++ classes/mmi/api.php
@@ -1,15 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-abstract class MMI_API extends Kohana_MMI_API
-{
-    /**
-     * Set a custom useragent string.
-     *
-     * @return  void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-        $this->_useragent = 'Mozilla/5.0 (compatible; Me Make It; '.URL::base(FALSE, TRUE).')';
-    }
-}
+abstract class MMI_API extends Kohana_MMI_API {}
