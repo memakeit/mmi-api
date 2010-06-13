b8a9fdb0bed86e46c5865dcde317ee4f930efe9b
Shawn Sprehe <memakeit@gmail.com>
2010-06-13 12:10:45 -0600

    refactored _build_url method

--- classes/kohana/mmi/api/readernaut.php
+++ classes/kohana/mmi/api/readernaut.php
@@ -17,11 +17,12 @@ class Kohana_MMI_API_Readernaut extends MMI_API
     /**
      * Build the request URL.
      *
+     * @param   string  the base URL
      * @param   string  the path portion of the URL
      * @return  string
      */
-    protected function _build_url($path)
+    protected function _build_url($url, $path)
     {
-        return "{$this->_api_url}{$this->_format}/{$path}";
+        return "$url{$this->_format}/$path";
     }
 } // End Kohana_MMI_API_Readernaut
