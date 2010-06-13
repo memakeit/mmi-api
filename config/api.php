708b57d3da9b65482246db1d99bbf49e7921c6e8
Shawn Sprehe <memakeit@gmail.com>
2010-06-12 18:43:32 -0600

    updated api config

--- config/api.php
+++ config/api.php
@@ -3,25 +3,37 @@
 // API configuration
 return array
 (
-    'delicious' => array
+    'api' => array
     (
+        'connect_timeout' => 5,
+        'decode' => TRUE,
+        'decode_as_array' => TRUE,
+        'format' => MMI_API::FORMAT_JSON,
+        'ssl_verifypeer' => FALSE,
+        'timeout' => 30,
+        'useragent' => 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3',
     ),
-    'digg' => array
-    (
-    ),
-    'facebook' => array
-    (
-    ),
-    'flickr' => array
-    (
-    ),
+
     'github' => array
     (
+        'api' => array('api_url' => 'https://github.com/api/v2/'),
+        'auth' => array
+        (
+            'username' => 'XXXXXXXXXXXXXXXXXXXX',
+            'password' => 'XXXXXXXXXXXXXXXXXXXX',
+        ),
+        'custom' => array
+        (
+            'curl_options'  => array
+            (
+                'remove'    => array(),
+                'add'       => array(),
+            ),
+            'http_headers'  => array
+            (
+                'remove'    => array(),
+                'add'       => array('X-Powered-By' => 'Me Make It'),
+            )
+        ),
     ),
-    'readernaut' => array
-    (
-    ),
-    'twitter' => array
-    (
-    )
 );
