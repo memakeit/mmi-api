c09b85b8ff5d095e8e057f228cc2581ca14fc3fa
Shawn Sprehe <memakeit@gmail.com>
2010-06-12 18:35:49 -0600

    added lastfm api

---
+++ classes/kohana/mmi/api/lastfm.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Make LastFM API calls.
+ *
+ * @package     MMI API
+ * @author      Me Make It
+ * @copyright   (c) 2010 Me Make It
+ * @license     http://www.memakeit.com/license
+ */
+class Kohana_MMI_API_LastFM extends MMI_API
+{
+    /**
+     * @var string the service name
+     */
+    protected $_service = MMI_API::SERVICE_LASTFM;
+} // End Kohana_MMI_API_LastFM
