41893bdaf1f1a93fea01b7b7d8056261a6b4c978
Shawn Sprehe <memakeit@gmail.com>
2010-06-17 10:43:27 -0600

    added support for digg

---
+++ classes/kohana/mmi/oauth/verification/digg.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Verify Digg authorization.
+ *
+ * @package     MMI API
+ * @author      Me Make It
+ * @copyright   (c) 2010 Me Make It
+ * @license     http://www.memakeit.com/license
+ */
+class Kohana_MMI_OAuth_Verification_Digg extends MMI_OAuth_Verification
+{
+    /**
+     * @var string the service name
+     */
+    protected $_service = MMI_Social::DIGG;
+} // End Kohana_MMI_OAuth_Verification_Digg
