569d8ac6e65f0f4e73cd732990b1917643b32d6f
Shawn Sprehe <memakeit@gmail.com>
2010-06-19 11:13:47 -0600

    changed MMI_OAuth_Verification from an abstract ot a concrete class

--- classes/mmi/oauth/verification.php
+++ classes/mmi/oauth/verification.php
@@ -1,3 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-abstract class MMI_OAuth_Verification extends Kohana_MMI_OAuth_Verification {}
+class MMI_OAuth_Verification extends Kohana_MMI_OAuth_Verification {}
