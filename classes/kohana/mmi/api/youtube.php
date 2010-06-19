8faf67179eba1bf53c1660a86dc04294319ec2ad
Shawn Sprehe <memakeit@gmail.com>
2010-06-19 11:20:44 -0600

    inherit from the google api class.  updated comments.

--- classes/kohana/mmi/api/youtube.php
+++ classes/kohana/mmi/api/youtube.php
@@ -1,14 +1,15 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
  * Make YouTube API calls.
+ * Response formats: Atom, JSON, RSS
  *
  * @package     MMI API
  * @author      Me Make It
  * @copyright   (c) 2010 Me Make It
  * @license     http://www.memakeit.com/license
- * @link        http://developer.linkedin.com/community/apis
+ * @link        http://code.google.com/apis/youtube/2.0/reference.html
  */
-class Kohana_MMI_API_YouTube extends MMI_API_OAuth
+class Kohana_MMI_API_YouTube extends MMI_API_Google
 {
     /**
      * @var string the service name
