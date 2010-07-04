7edaabcfbeda9ada0d69867ba0b1e3ba449c5a1e
Shawn Sprehe <memakeit@gmail.com>
2010-07-04 09:37:16 -0600

    moved curl-related classes to mmi-curl

--- classes/mmi/curl/response.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class MMI_Curl_Response extends Kohana_MMI_Curl_Response {}
--- classes/mmi/http.php
+++
@@ -1,3 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class MMI_HTTP extends Kohana_MMI_HTTP {}
